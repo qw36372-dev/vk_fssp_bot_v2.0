6d90c0e149f9091e6070f04d1d83abbba2f2ee0d
qw36372-dev <qw36372@gmail.com>
2026-02-20 09:20:02 +0300

    Add files via upload

---
+++ questions/__init__.py
@@ -0,0 +1,18 @@
+"""
+Пакет с закрытыми JSON‑библиотеками вопросов.
+Файлы недоступны пользователю, импортируются только ботом.
+"""
+
+__all__ = [
+    "oupds",
+    "ispolniteli",
+    "aliment",
+    "doznanie",
+    "rozyisk",
+    "prof",
+    "oko",
+    "informatika",
+    "kadry",
+    "bezopasnost",
+    "upravlenie",
+]
